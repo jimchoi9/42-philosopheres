a7617281340de336058719316309ca2657995853
jimchoi <jimchoi@student.42seoul.kr>
2024-06-20 21:03:12 +0900

    0620

---
+++ test.c
@@ -0,0 +1,82 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define N 5  // 철학자 수
+#define LEFT(i) ((i + N - 1) % N)
+#define RIGHT(i) ((i + 1) % N)
+pthread_mutex_t mutex[5];
+int cnt = 0;
+
+
+
+typedef struct Philosopher {
+    int id;
+} Philosopher;
+
+void think(Philosopher* p)
+{
+	printf("%d is thinking\n",p->id);
+}
+void eat(Philosopher* p)
+{
+	printf("%d is eating\n", p->id);
+}
+void take_forks(Philosopher* p) {
+    pthread_mutex_lock(&mutex[p->id]);  // 왼쪽 포크 뮤텍스 획득
+    pthread_mutex_lock(&mutex[RIGHT(p->id)]);  // 오른쪽 포크 뮤텍스 획득
+}
+
+void put_forks(Philosopher* p) {
+    pthread_mutex_unlock(&mutex[p->id]);  // 왼쪽 포크 뮤텍스 반환
+    pthread_mutex_unlock(&mutex[RIGHT(p->id)]);  // 오른쪽 포크 뮤텍스 반환
+}
+
+void* philosopher(void* arg) {
+    Philosopher* p = (Philosopher*)arg;
+	int i = 0;
+	if (p->id % 2 == 0)
+		usleep(100000);
+    while (i < 6) {
+        think(p);
+        take_forks(p);
+        eat(p);
+		i++;
+        put_forks(p);
+		usleep(100000);
+    }
+	printf("done %d\n", p->id);
+}
+
+
+int main()
+{
+	 Philosopher philosophers[5];
+    pthread_t thread[5];
+
+	for(int i = 0; i < 5; i++)
+	{
+		pthread_mutex_init(&mutex[i],NULL);
+		philosophers[i].id = i;
+		
+	}
+	for(int i = 0; i < 5; i++)
+	{
+		pthread_create(&thread[i], NULL, philosopher, &philosophers[i]);
+		
+	}
+	for(int i = 0; i < 5; i++)
+	{
+		pthread_join(thread[i], NULL);
+		
+	}
+	for(int i = 0; i < 5; i++)
+	{
+		pthread_mutex_destroy(&mutex[i]);
+		
+	}
+
+	printf("done !\n");
+
+}
