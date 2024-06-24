0b65e1f3c378fb89dce5415e7b720ed94856f2d0
jimchoi <jimchoi@student.42seoul.kr>
2024-06-24 16:27:07 +0900

    aa

--- test.c
+++ test.c
@@ -7,6 +7,8 @@
 #define LEFT(i) ((i + N - 1) % N)
 #define RIGHT(i) ((i + 1) % N)
 pthread_mutex_t mutex[5];
+pthread_mutex_t start;
+
 int cnt = 0;
 
 
@@ -25,7 +27,9 @@ void eat(Philosopher* p)
 }
 void take_forks(Philosopher* p) {
     pthread_mutex_lock(&mutex[p->id]);  // 왼쪽 포크 뮤텍스 획득
+	printf("%d is taking left forks\n",p->id);
     pthread_mutex_lock(&mutex[RIGHT(p->id)]);  // 오른쪽 포크 뮤텍스 획득
+	printf("%d is taking right forks\n",p->id);
 }
 
 void put_forks(Philosopher* p) {
@@ -34,17 +38,19 @@ void put_forks(Philosopher* p) {
 }
 
 void* philosopher(void* arg) {
+    pthread_mutex_lock(&start);  // 왼쪽 포크 뮤텍스 획득
+    pthread_mutex_unlock(&start);  // 오른쪽 포크 뮤텍스 
     Philosopher* p = (Philosopher*)arg;
 	int i = 0;
 	if (p->id % 2 == 0)
 		usleep(100000);
     while (i < 6) {
-        think(p);
         take_forks(p);
         eat(p);
 		i++;
         put_forks(p);
 		usleep(100000);
+        think(p);
     }
 	printf("done %d\n", p->id);
 }
@@ -52,8 +58,8 @@ void* philosopher(void* arg) {
 
 int main()
 {
-	 Philosopher philosophers[5];
-    pthread_t thread[5];
+	Philosopher philosophers[5];
+	pthread_t thread[5];
 
 	for(int i = 0; i < 5; i++)
 	{
@@ -63,7 +69,10 @@ int main()
 	}
 	for(int i = 0; i < 5; i++)
 	{
+
+    pthread_mutex_lock(&start);  // 왼쪽 포크 뮤텍스 획득
 		pthread_create(&thread[i], NULL, philosopher, &philosophers[i]);
+    pthread_mutex_unlock(&start);  // 오른쪽 포크 뮤텍스 반환
 		
 	}
 	for(int i = 0; i < 5; i++)
